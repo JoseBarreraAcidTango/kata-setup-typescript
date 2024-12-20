eb9156ea3db8f52f0b28785034eb137b3516b520
JoseBarreraAcidTango <jose@acidtango.com>
2024-12-20 11:32:58 -0500

    feat: enhance Garden class with light management methods and update tests

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -2,8 +2,28 @@ import { describe, it, expect } from "vitest"
 import { Garden } from "./main.js"
 
 describe("Default test", () => {
-  it("should work", () => {
-    const result = new Garden()
-    expect(result.lightsUpCount).toBe(0)
+  it("initialize with 0 lights on", () => {
+    const result = new Garden(1000, 1000)
+    expect(result.lightsUp).toBe(0)
+  })
+
+  describe("Garden light instructions", () => {
+    it("should follow the instructions and count the lights correctly", () => {
+      const garden = new Garden(1000, 1000)
+
+      garden.turnOn(887, 9, 959, 629)
+      garden.turnOn(454, 398, 844, 448)
+      garden.turnOff(539, 243, 559, 965)
+      garden.turnOff(370, 819, 676, 868)
+      garden.turnOff(145, 40, 370, 997)
+      garden.turnOff(301, 3, 808, 453)
+      garden.turnOn(351, 678, 951, 908)
+      garden.toggle(720, 196, 897, 994)
+      garden.toggle(831, 394, 904, 860)
+
+      console.log(garden.lightsUpCount())
+      console.log(garden.lightsUp)
+      expect(garden.lightsUpCount()).toBe(231000)
+    })
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,55 @@
 type Light = { postion: number; isActive: number }
 
 export class Garden {
-  light!: Light[][]
+  lights!: boolean[][]
   lightsUp!: number
 
-  constructor() {
-    this.light = [[{ postion: 0, isActive: 0 }]]
+  constructor(numberOfRows: number, numberOfColumns: number) {
+    this.lights = Array(numberOfRows).fill(Array(numberOfColumns).fill(0))
     this.lightsUp = 0
   }
 
+  turnOn(x1: number, y1: number, x2: number, y2: number): void {
+    for (let i = x1; i <= x2; i++) {
+      for (let j = y1; j <= y2; j++) {
+        if (!this.lights[i][j]) this.lightsUp += 1
+        this.lights[i][j] = true
+      }
+    }
+  }
+
+  turnOff(x1: number, y1: number, x2: number, y2: number): void {
+    for (let i = x1; i <= x2; i++) {
+      for (let j = y1; j <= y2; j++) {
+        if (this.lights[i][j]) this.lightsUp -= 1
+        this.lights[i][j] = false
+      }
+    }
+  }
+
+  toggle(x1: number, y1: number, x2: number, y2: number): void {
+    for (let i = x1; i <= x2; i++) {
+      for (let j = y1; j <= y2; j++) {
+        if (this.lights[i][j]) {
+          this.lightsUp -= 1
+          this.lights[i][j] = false
+        } else {
+          this.lightsUp += 1
+          this.lights[i][j] = true
+        }
+      }
+    }
+  }
+
   lightsUpCount(): number {
-    return this.lightsUp
+    let count = 0
+    for (let row of this.lights) {
+      for (let light of row) {
+        if (light) {
+          count++
+        }
+      }
+    }
+    return count
   }
 }
