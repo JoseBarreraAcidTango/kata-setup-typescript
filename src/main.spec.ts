75fadad468168546f6b1270db6626663d317e7bb
JoseBarreraAcidTango <jose@acidtango.com>
2024-12-20 11:02:12 -0500

    red: first iteration

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,10 +1,9 @@
 import { describe, it, expect } from "vitest"
-import { myFunction } from "./main.js"
+import { Garden } from "./main.js"
 
 describe("Default test", () => {
   it("should work", () => {
-    const result = myFunction()
-
-    expect(result).toBe(true)
+    const result = new Garden()
+    expect(result.lightsUpCount).toBe(0)
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
-export function myFunction() {
-  return true
+type Light = { postion: number; isActive: number }
+
+export class Garden {
+  light!: Light[][]
+  lightsUp!: number
+
+  constructor() {
+    this.light = [[{ postion: 0, isActive: 0 }]]
+    this.lightsUp = 0
+  }
+
+  lightsUpCount(): number {
+    return this.lightsUp
+  }
 }
